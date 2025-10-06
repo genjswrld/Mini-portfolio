840a14f36097321dab23d86665a3d243fb23bf01
Gav <school4gabsco@gmail.com>
2025-10-06 12:13:40 +0100

    Color edittng

--- 01 Drawing/sketch.js
+++ 01 Drawing/sketch.js
@@ -1,22 +1,27 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(640, 480); 
+  background(50);    
+  // noStroke();              
 }
 
-function draw() {
-  let c = color(255, 203, 165);
-  let c2 = color(65)
-  let c3 = color (251, 163, 114)
-  background(50);
-  
-  
-  fill(c);
-  noStroke();
-  circle(200, 200, 300);
-  
-  fill(c2);
-  square(250, 150, 50);
-  square(100, 150, 50);
-  
-  fill (c3);
-  rect(170, 200, 55, 50, 10);
+function draw(){
+  let c  = color(255,203,165);
+  let c2 = color(251,163,114);
+
+
+
+    fill(c); 
+    rect(0, 0, 100, 100); // x, y, w, h
+    
+    fill(c2);
+		rect(50, 50, 100, 100); 
+
+		fill(128)
+		rect(100, 100, 100, 100);
+		
+		fill(196);
+		rect(150, 150, 200, 200);
+
+		fill(220);
+		rect(300, 300, 400, 400); 
 }
