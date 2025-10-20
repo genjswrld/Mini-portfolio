f8599da95dc34e3d0ff43971a98f37e20fb5e76f
Gav <school4gabco@gmail.comm>
2025-10-20 10:42:07 +0100

    editw

---
+++ 04 multiple prompts/sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  circle(50, 50, 50);
+  circle(50, 60, 50);
+  circle(50, 70, 50);
+}
+  for(let i=0; i<=100; i++){
+	circle(i, i, 30);
+}
